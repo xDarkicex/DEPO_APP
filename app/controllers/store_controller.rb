d86b74ed488f06a8d93d261ab42e57d9d96add91
Gentry Rolofson <xdarkicex@gmail.com>
2016-05-11 23:12:39 -0700

    Bull Shit Commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,15 +1,17 @@
 class StoreController < ApplicationController
     skip_before_action :authorize
-    
+
     include CurrentCart
     before_action :set_cart
     def index
+
       if params[:set_locale]
         redirect_to store_url(locale: params[:set_locale])
       else
       	@products = Product.order(:title)
-      end
 
+      end
+def increment_counter
       	if session[:counter].nil?
 
       		@visit = 1
@@ -21,4 +23,9 @@ class StoreController < ApplicationController
 
       	session[:counter] = @visit
     end
+    @time = Time.now
+    @count = increment_counter
+    @display_count = "<div>This is your #{@count} at #{@time}</div>".html_safe
+  end
+
 end
